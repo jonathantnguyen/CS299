cc77ee59df63205ad07107d0bdd146a6a6b806bf
jonathantnguyen <jonnytnguyen@gmail.com>
2018-01-10 17:52:31 -0800

    update

--- CS299/LLL/Level_1/CS299_list.cpp
+++ CS299/LLL/Level_1/CS299_list.cpp
@@ -2,29 +2,38 @@
 
 int display_allButFirst(node * head)
 {
-	node * current = head;
-	int i = 0;
+	
+
+	if (!head) //Check if head is NULL/EMPTY LIST
+	{
+		return 0; //If so, kick out
+	}
+
+	node * current = head; //Setting the current pointer to where the head is pointing
+	int i = 0; 
 	int value;
 	int value2;
-	if (head ==NULL)
-	{
-		return 0;
-	}
-	value = head->data;
-	current = current->next;
-	value2 = current->data;
+
+	value = head->data;	//we place the head data value into a dummy variable
+	current = current->next; //Traveresing
+	//value2 = current->data; //Tak
 
 	while(current != NULL)
 	{	
 		value2= current->data;
-		if (value != value2)
-		{
+		if (value != value2)	//If the head value doesn't equal current node value
+		{						//print the current node data value
 			cout << value2 << endl;
-			i++;
+			i++;					// increment counter 
 		}
 		current = current->next;
 		
 	}
 
 return i;
+}
+
+int dataGTLastNode(node * head)
+{
+
 }
