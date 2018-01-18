349e221188a7a74df460724696dbf61176b76213
jonathantnguyen <jonnytnguyen@gmail.com>
2018-01-17 17:53:57 -0800

    a

--- CS299/LLL/Level_2/CS299_list.cpp
+++ CS299/LLL/Level_2/CS299_list.cpp
@@ -1,6 +1,6 @@
 #include "CS299_list.h"
 
-int traverse (node * current, node * head, int match);
+int traverse (node * current, int match);
 
 bool find(node * head, int match)
 {
@@ -8,11 +8,11 @@ bool find(node * head, int match)
 		return 0;
 
 	node * current = head;
-	if(traverse(current, head, match));
+	if(traverse(current, match));
 		return 1;
 }
 
-int traverse(node * current, node *head, int match)
+int traverse(node * current, int match)
 {
 	while (current != NULL)
 	{
