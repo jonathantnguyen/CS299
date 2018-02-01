8cfd8eb6c94561f59089dcb6b320a89985b7a793
jonathantnguyen <jonnytnguyen@gmail.com>
2018-01-31 17:12:03 -0800

    New Assignment

---
+++ CS299/DLL/Level_1/CS299_dlist.cpp
@@ -0,0 +1,34 @@
+#include "CS299_dlist.h"
+
+void find_tail(node * current, node * tail, node * & head);
+
+int remove_larger(node * & head)
+{
+	if(!head)
+		return 0;
+
+	node * current = head;
+	node * tail;
+
+	int first_val = head->data;
+	printf("%d \n",first_val); 
+
+	find_tail(current, tail, head);
+
+	current = head;
+
+
+
+}
+
+void find_tail(node * current, node * tail, node * & head)
+{
+	while (current->next != NULL)
+	{
+		current = current->next;
+
+		if(current->next == NULL)
+			tail = current;
+	}
+
+}
