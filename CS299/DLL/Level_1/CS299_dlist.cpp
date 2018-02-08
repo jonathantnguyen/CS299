ebc5c0c7ca5bb73eb676a697507ec6119289f3ea
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-07 20:39:56 -0800

    a

--- CS299/DLL/Level_1/CS299_dlist.cpp
+++ CS299/DLL/Level_1/CS299_dlist.cpp
@@ -40,12 +40,11 @@ int compare_delete(int i, node * & current, node * & tail, node * & target, node
 		if((target->data < current->data) && target != current)
 		{	temp = current->previous;
 			cout << current->data << " Is greater" << endl;
-
+			i++;
 			return compare_delete(i, current->next, tail, target, temp, head);
 			delete current;
 			current = temp;
 			temp = temp->previous;
-			i++;
 		}
 		else
 		{
