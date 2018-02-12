65faf8a2cceb29b052cec9c899b9fd613eed65fc
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-11 17:17:25 -0800

    a

--- CS299/DLL/Level_2/CS299_dlist.cpp
+++ CS299/DLL/Level_2/CS299_dlist.cpp
@@ -19,19 +19,19 @@ int swap (node * & head, node * & current, node * & temp)
 	if(current->next == NULL)
 	{
 		cout << "Last Node: " <<current->data << endl;
+		cout << "2nd Last Node: " <<current->previous->data << endl;
 		/*
 		temp = current->previous;
 		temp->next = NULL;
 		temp->previous->next = current;
-		current->previous = temp->previous;
+		current->previous = temp->previous;// NEEDS FIXING
 		temp->previous = current;
 		current->next = temp;
 		int a = temp->data;
 		int b = current->data;
 		int i = a + b;
-		return i;
 		*/
-		return current->data;
+		return i;
 	}
 	return swap(head, current->next, temp);
 }
