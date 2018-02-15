a1c296c3c65a173a43b60ee18c470f2a2468ed09
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-14 18:26:03 -0800

    a

--- CS299/ARR/Level_1/CS299_arr.cpp
+++ CS299/ARR/Level_1/CS299_arr.cpp
@@ -7,7 +7,7 @@ int find_longest(node ** head)
 	
 	int k = 0;
 	int * count = 0;				
-	for (int i = 0; i < SIZE; ++i)
+	for (int i = 0; i < SIZE; i++)
 	{	int j = 0;
 		node * current = head[i];
 		while(current)
@@ -20,7 +20,7 @@ int find_longest(node ** head)
 		if(k < j)
 			k = j;
 	}
-	for (int i = 0; i < SIZE; ++i)
+	for (int i = 0; i < SIZE; i++)
 	{
 		if(count[i] == k)
 			cout << "Index " << i << ": " << endl;
