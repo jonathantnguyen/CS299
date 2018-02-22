e4f643bb68cfba5b3283b31492547769ac621bcc
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-21 17:28:02 -0800

    a

--- CS299/ARR/Level_1/CS299_arr.cpp
+++ CS299/ARR/Level_1/CS299_arr.cpp
@@ -100,6 +100,11 @@ int remove_allt(node ** head)
 				current = nxt_cur;
 				nxt_cur = nxt_cur->next;
 				k++;
+				if (!nxt_cur)
+				{
+					delete nxt_cur;
+					k++;
+				}
 			}
 		}
 		head[i]->next = NULL;
