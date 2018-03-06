df382e40a4f63fe3bad6ae5167661f40748c82a0
jonathantnguyen <jonnytnguyen@gmail.com>
2018-03-05 17:07:02 -0800

    a

--- CS299/BST/Level_1/CS299_main.cpp
+++ CS299/BST/Level_1/CS299_main.cpp
@@ -8,9 +8,12 @@ int main()
 
     /*  PLACE YOUR FUNCTION CALL HERE */
     int count;
+    int sum;
 
-    count = count_greater_than(root);
-    cout << "Number of nodes with data greater than the root's data: " << count << endl;
+    //count = count_greater_than(root);
+    //cout << "Number of nodes with data greater than the root's data: " << count << endl;
+    sum = add_all_leafs(root);
+    cout << "The sum of every leaf's data: " << sum << endl;
 
     display_all(root);
 
